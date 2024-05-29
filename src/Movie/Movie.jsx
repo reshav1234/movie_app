17ccde71f96b93064f2f78da6ef64f18653554b6
reshav1234 <thapareshav50@gmail.com>
2024-05-29 13:20:45 +0545

    axios added for url fetch

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,39 +1,41 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MovieList from './MovieList';
 import './movie.css'
 
+import axios from 'axios';
+
 const Movie = () => {
-	// State to store movies from the API
-    const [movies, setMovies] = useState([]);
+  // State to store movies from the API
+  const [movies, setMovies] = useState([]);
 
-	const getMovieRequest = async () => {
-		// Fetching API from OMDB API
-		const url = `https://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+  const getMovieRequest = async () => {
+    // Fetching API from OMDB API
+    const url = `https://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
 
-		const response = await fetch(url);
-		const responseJson = await response.json();
+    const response = await axios.get(url);
+    const responseJson = await response.data;
 
-        if (responseJson.Search && responseJson.Search.length >= 3) {
-            const selectedMovies = responseJson.Search.slice(0, 3); // Extract the first three movies
-            setMovies(selectedMovies);
-        } else {
-            setMovies([]); // Handle if there are no search results or less than three movies
-        }
-	};
+    if (responseJson.Search && responseJson.Search.length >= 3) {
+      const selectedMovies = responseJson.Search.slice(0, 3); // Extract the first three movies
+      setMovies(selectedMovies);
+    } else {
+      setMovies([]); // Handle if there are no search results or less than three movies
+    }
+  };
 
-	useEffect(() => {
-		// Fetch url in first render 
-		getMovieRequest();
-	}, []);
+  useEffect(() => {
+    // Fetch url in first render 
+    getMovieRequest();
+  }, []);
 
-	return (
-		<div className='container-fluid movie-app'>
-			<div className='row_01'>
-				<MovieList movies={movies} />
-			</div>
-		</div>
-	);
+  return (
+    <div className='container-fluid movie-app'>
+      <div className='row_01'>
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
 }
 
 export default Movie
