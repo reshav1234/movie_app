6a5bd84789ce2737031785be706f2273ca3f271f
reshav1234 <thapareshav50@gmail.com>
2024-12-06 18:17:06 +0545

    new commit

--- src/Movie/MovieList.jsx
+++ src/Movie/MovieList.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 import './movie.css'
 
 const MovieList = (props) => {
-	// Using map function to display 3 moivie list from the API
-	// Displays Poster and movie title
-	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<Link to={`/movie/${movie.imdbID}`} key={index}>
-					<div className='image-container d-flex justify-content-start m-3'>
-            			<img src={movie.Poster} alt='movie' />
-            			<p>{movie.Title}</p>
-          			</div>
-				</Link>
-			))}
-		</>
-	);
+  // Using map function to display 3 moivie list from the API
+  // Displays Poster and movie title
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <Link to={`/movie/${movie.imdbID}`} key={index}>
+          <div className='image-container d-flex justify-content-start m-3'>
+            <img src={movie.Poster} alt='movie' />
+            <p>{movie.Title}</p>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
